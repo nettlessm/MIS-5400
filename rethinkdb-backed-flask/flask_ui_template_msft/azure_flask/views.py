897a75c99a5a8b44110b952a77dd8625f2f59f9a
McKelly Peart <mckelly@thepearts.com>
2018-11-28 00:46:01 -0700

    Form to add data.

--- rethinkdb-backed-flask/flask_ui_template_msft/azure_flask/views.py
+++ rethinkdb-backed-flask/flask_ui_template_msft/azure_flask/views.py
@@ -4,11 +4,10 @@ Routes and views for the flask application.
 import pyodbc
 import requests as r
 from datetime import datetime
-from flask import render_template, send_file
+from flask import render_template, send_file, request, abort
 from azure_flask import app
 import json
 import os
-from pymongo import MongoClient
 from bson.json_util import dumps, loads
 from bson.objectid import ObjectId
 import matplotlib.pyplot as plt
@@ -48,7 +47,7 @@ def about():
     )
 
 
-@app.route('/data')
+@app.route('/data',  methods=['GET'])
 def data():
     """Renders the data page."""
     raw_data = get_fused_news_data()
@@ -58,6 +57,15 @@ def data():
         message='Give me your data.',
         image_name='some_image',
         news_data=loads(raw_data.content)
+    )
+
+@app.route('/add_data_form', methods=['GET'])
+def add_data_form():
+    """Presents form to add data."""
+    return render_template(
+        'add_data_form.html',
+        title='Add Some Data',
+        message='Give me your data.'
     )
 
 @app.route('/image/<some_image>')
@@ -75,3 +83,16 @@ def image_render(some_image):
 
 def get_fused_news_data():
     return r.get("http://localhost:5001/api/v1/fusednews")
+
+
+@app.route('/add_fused_news_data',methods=["POST"])
+def add_fused_news_data():
+    fused_news_item = {'headline':request.form['headline'],'url':request.form['url']}
+
+    try:
+        result = r.post('http://localhost:5001/api/v1/fusednews', json=fused_news_item, )
+        return 'success', 200
+    except Exception as e:
+        return abort(500)
+
+
